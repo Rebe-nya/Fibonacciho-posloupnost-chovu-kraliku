a9adde8aeb5021427a8e8767a8fbfc21a9656ddf
Filip Slabina <filip.slabinaf@gmail.com>
2025-02-04 09:04:21 +0100

    Generation

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // Funkce pro generování Fibonacciho posloupnosti
 function generateFibonacci(n) {
-
+    let a = 1, b = 1, temp;
+    let i = 0;
+    while (i <= n) {
+        let j = 0;
+        let str = "";
+        while (j<a){
+            str += "🐇"
+            j++;
+        }
+        console.log(a);
+        temp = a + b;
+        a = b;
+        b = temp;
+        i++;
+    }
 }
 
 
