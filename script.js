ffc4ef61d1a13a7aa8944e2691f235c13c7b2aea
Rebe-nya <filip.slabinaf@gmail.com>
2025-02-09 20:56:00 +0100

    Size for rabbits

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+/* 
+Minimální velikost pro králíky:
+https://www.pdsa.org.uk/pet-help-and-advice/looking-after-your-pet/rabbits/creating-the-ideal-home-for-your-rabbits
+10ft x 6ft x 3ft/3m x 2m x 1m
+3 * 2 = 6 m2 (jeden králík)
+*/
+
 // Funkce pro generování Fibonacciho posloupnosti
 function generateFibonacci(n) {
-    const divFibonacci = document.getElementById("fibonacci");
     divFibonacci.innerHTML = "";
-    let a = 1, b = 1, temp;
+    let a = 1, b = 1, num = 0, temp;
     let i = 0;
     while (i <= n) {
         const divRabbit = document.createElement("div");
@@ -15,22 +21,36 @@ function generateFibonacci(n) {
         divRabbit.classList.add("rabbit");
         divRabbit.innerHTML = str;
         divFibonacci.appendChild(divRabbit);
+
+        num += a * 2;
+
         temp = a + b;
         a = b;
         b = temp;
         i++;
     }
+    return num;
 }
 
 // Spuštění generování Fibonacciho posloupnosti s 6 prvky
-const n = document.getElementById("months");
+const divFibonacci = document.getElementById("fibonacci");
+const n = document.getElementById("meters");
 const btn = document.getElementById("btn");
 
 btn.addEventListener("click", function() {
-    const months = parseInt(n.value);
-    if (!isNaN(months) && months > 0) {
-        generateFibonacci(months);
-    } else {
-        alert("Please enter a valid positive number of months.");
+    let meters = parseInt(n.value);
+
+    if (meters < 12) {
+        divFibonacci.innerHTML = "Pár králíků se nemůže vejít na pozemek";
     }
+    else {
+        meters = (meters / 12) - 1;
+        num = generateFibonacci(meters);
+        console.log("Králíci =" + " " + num);
+        num = num * 6;
+        console.log("Rozměr =" + " " + num);
+        console.log("Meters =" + " " + meters);
+        generateFibonacci(meters);
+    }
+    console.log("");
 });
